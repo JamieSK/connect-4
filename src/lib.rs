a9b09ddb353319ff882ba0a516331c039fe1e5b3
Jamie Klein <jamiesklein@gmail.com>
2018-05-31 07:53:38 +0100

    Can play first go as red.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,35 @@
-pub struct Connect4 {}
+pub struct Connect4 {
+    board: String,
+}
+
+pub enum Player {
+    Red,
+}
 
 impl Connect4 {
     pub fn new() -> Connect4 {
-        Connect4 {}
+        Connect4 {
+            board: String::from("\n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n"),
+        }
     }
 
     pub fn to_string(&self) -> String {
-        String::from("\n\
+        format!("{}", self.board)
+    }
+
+    pub fn play(&mut self, _player: Player, _column: usize) {
+        self.board = String::from("\n\
  | | | | | | \n\
  | | | | | | \n\
  | | | | | | \n\
  | | | | | | \n\
  | | | | | | \n\
- | | | | | | \n")
+ | | |\x1b[31mO\x1b[0m| | | \n");
     }
 }
--- tests/spec.rs
+++ tests/spec.rs
@@ -1,6 +1,7 @@
 extern crate connect_4;
 
 use connect_4::Connect4;
+use connect_4::Player;
 
 #[test]
 fn it_starts_blank() {
@@ -12,4 +13,17 @@ fn it_starts_blank() {
  | | | | | | \n\
  | | | | | | \n\
  | | | | | | \n", game.to_string());
+}
+
+#[test]
+fn can_play_first_go_as_red() {
+    let mut game = Connect4::new();
+    game.play(Player::Red, 4);
+    assert_eq!("\n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | | | | | \n\
+ | | |\x1b[31mO\x1b[0m| | | \n", game.to_string());
 }
